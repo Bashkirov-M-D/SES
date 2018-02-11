4eddc3dc5f747d7e219fc095a96a3552b014bcbc
Michail <forbidder-s@mail.ru>
2018-02-11 09:29:45 +0200

    2

--- core/src/com/mfitss/idletd/Main.java
+++ core/src/com/mfitss/idletd/Main.java
@@ -1,14 +1,15 @@
 package com.mfitss.idletd;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MAIN extends ApplicationAdapter {
+public class Main extends Game {
     SpriteBatch batch;
     Texture img;
+
 
     @Override
     public void create() {
--- desktop/src/com/mfitss/idletd/desktop/DesktopLauncher.java
+++ desktop/src/com/mfitss/idletd/desktop/DesktopLauncher.java
@@ -2,12 +2,12 @@ package com.mfitss.idletd.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mfitss.idletd.MAIN;
+import com.mfitss.idletd.Main;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
         System.setProperty("user.name", "Public");
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new MAIN(), config);
+        new LwjglApplication(new Main(), config);
     }
 }
