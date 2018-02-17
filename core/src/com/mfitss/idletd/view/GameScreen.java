85fe41a7a4b883c61848a57ad02554dd39a785b5
Michail <forbidder-s@mail.ru>
2018-02-17 17:49:40 +0200

    2

--- core/src/com/mfitss/idletd/view/GameScreen.java
+++ core/src/com/mfitss/idletd/view/GameScreen.java
@@ -20,7 +20,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(texture, 0, 0);
