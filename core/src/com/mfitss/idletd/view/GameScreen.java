b9d29ca0fbfb03e9d4cfb5ca0ddfa1317e4af142
Michail <forbidder-s@mail.ru>
2018-02-19 22:15:07 +0200

    2

--- core/src/com/mfitss/idletd/view/GameScreen.java
+++ core/src/com/mfitss/idletd/view/GameScreen.java
@@ -4,16 +4,22 @@ package com.mfitss.idletd.view;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen implements Screen {
+    OrthographicCamera camera;
+
     private SpriteBatch batch;
 
     private Texture texture;
 
     @Override
     public void show() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 16, 9);
+
         batch = new SpriteBatch();
         texture = new Texture("103419216_1310118147_padme_by_taho.jpg");
     }
@@ -22,8 +28,10 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.draw(texture, 0, 0);
+        batch.draw(texture, 0, 0, 3, 6);
         batch.end();
     }
 
